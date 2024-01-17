952ac87dde963cafac3d61912c2eacdb6d9d5b09
PJane24 <jhoannacpd@gmail.com>
2024-01-17 21:22:07 +0100

    v2 portfolio act

--- portfolio/js/functions.js
+++ portfolio/js/functions.js
@@ -1,13 +1,25 @@
 window.onload = () => {
     const titulo = document.querySelector("#titulo");
+    const menu = document.querySelector("nav");
+
+    window.addEventListener('scroll', function(){
+        menu.classList.toggle('active',window.scrollY > 0)
+    })
 
     titulo.addEventListener("click", () => {
-        if(titulo.innerHTML == "Jane"){
-            titulo.innerHTML = "Jhoanna Pereira";
+        if(titulo.innerHTML == "Jane."){
+            titulo.style.transition = "2s";
+            titulo.innerHTML = "Jhoanna Pereira.";
+            titulo.style.width = "16ch";
         }else{
-            titulo.innerHTML = "Jane";
+
+            
+            titulo.style.transition = "1s";
+            titulo.innerHTML = "Jane.";
+            titulo.style.width = "5ch";
         }
+
     })
     
-
 }
+
